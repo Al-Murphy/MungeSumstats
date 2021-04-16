6846ffb3a2e1d93d3e96b911bc96a40201427079
Al-Murphy <alanmurph94@hotmail.com>
2021-04-16 16:33:57 +0100

    Address 2 GB 32 windows tests

--- tests/testthat/test-no_chr_bp.R
+++ tests/testthat/test-no_chr_bp.R
@@ -9,19 +9,29 @@ test_that("Imputation of CHR and BP correctly", {
   sumstats_dt_missing[,CHR:=NULL]
   sumstats_dt_missing[,POS:=NULL]
   data.table::fwrite(x=sumstats_dt_missing, file=file, sep="\t")
-  #Run MungeSumstats code
-  reformatted <- MungeSumstats::format_sumstats(file,ref_genome="GRCh37")
-  res_dt <- data.table::fread(reformatted)
-  #correct names of MungeSumstats::eduAttainOkbay
-  names(sumstats_dt) <- c("SNP","CHR","BP","A1","A2","FRQ","Beta","SE","P")
-  #get order same
-  setkey(res_dt,SNP)
-  setkey(sumstats_dt,SNP)
-  #add CHR, BP to org
-  sumstats_dt[res_dt,CHR_der:=i.CHR]
-  sumstats_dt[res_dt,BP_der:=i.BP]
-  #remove any that weren't found in reference
-  sumstats_dt <- sumstats_dt[complete.cases(sumstats_dt),]
-  expect_equal(sumstats_dt$BP,sumstats_dt$BP_der)
-  expect_equal(sumstats_dt$CHR,sumstats_dt$CHR_der)
+  ## The following test uses more than 2GB of memory, which is more
+  ## than what 32-bit Windows can handle:
+  is_32bit_windows <- .Platform$OS.type == "windows" &&
+    .Platform$r_arch == "i386"
+  if (!is_32bit_windows) {
+    #Run MungeSumstats code
+    reformatted <- MungeSumstats::format_sumstats(file,ref_genome="GRCh37")
+    res_dt <- data.table::fread(reformatted)
+    #correct names of MungeSumstats::eduAttainOkbay
+    names(sumstats_dt) <- c("SNP","CHR","BP","A1","A2","FRQ","Beta","SE","P")
+    #get order same
+    setkey(res_dt,SNP)
+    setkey(sumstats_dt,SNP)
+    #add CHR, BP to org
+    sumstats_dt[res_dt,CHR_der:=i.CHR]
+    sumstats_dt[res_dt,BP_der:=i.BP]
+    #remove any that weren't found in reference
+    sumstats_dt <- sumstats_dt[complete.cases(sumstats_dt),]
+    expect_equal(sumstats_dt$BP,sumstats_dt$BP_der)
+    expect_equal(sumstats_dt$CHR,sumstats_dt$CHR_der)
+  }
+  else{
+    expect_equal(is_32bit_windows,TRUE)
+    expect_equal(is_32bit_windows,TRUE)
+  }
 })
