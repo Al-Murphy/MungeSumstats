c806ee14824b47fd79dfb82d3f5b82c7524780fd
Al-Murphy <alanmurph94@hotmail.com>
2021-09-15 07:33:56 +0100

    VCF Enhancements

---
+++ R/check_frq.R
@@ -0,0 +1,50 @@
+#' Ensure all SNPs have frq score above threshold
+#'
+#' @inheritParams format_sumstats
+#' @param log_files list of log file locations
+#' @return list containing sumstats_dt, the modified summary statistics data
+#' table object and the log file list
+#' @keywords internal
+check_frq <- function(sumstats_dt, path, FRQ_filter, log_folder_ind,
+                             check_save_out, tabix_index, nThread, log_files) {
+    FRQ <- NULL
+    col_headers <- names(sumstats_dt)
+    if ("FRQ" %in% col_headers && FRQ_filter > 0) {
+        message("Filtering SNPs based on FRQ.")
+        # use data table for speed
+        num_bad_ids <- nrow(sumstats_dt[FRQ < FRQ_filter, ])
+        if (num_bad_ids > 0) {
+            msg <- paste0(
+                formatC(num_bad_ids, big.mark = ","), " SNPs",
+                " are below the FRQ threshold of ", FRQ_filter,
+                " and will be removed."
+            )
+            message(msg)
+            # If user wants log, save it to there
+            if (log_folder_ind) {
+                name <- "frq_filter"
+                name <- get_unique_name_log_file(name = name, 
+                                                    log_files = log_files)
+                write_sumstats(
+                    sumstats_dt = sumstats_dt[FRQ < FRQ_filter, ],
+                    save_path =
+                        paste0(
+                            check_save_out$log_folder,
+                            "/", name,
+                            check_save_out$extension
+                        ),
+                    sep = check_save_out$sep,
+                    tabix_index = tabix_index,
+                    nThread = nThread
+                )
+                log_files[[name]] <-
+                    paste0(check_save_out$log_folder, "/", name, 
+                            check_save_out$extension)
+            }
+            sumstats_dt <- sumstats_dt[FRQ >= FRQ_filter, ]
+        }
+        return(list("sumstats_dt" = sumstats_dt, "log_files" = log_files))
+    } else {
+        return(list("sumstats_dt" = sumstats_dt, "log_files" = log_files))
+    }
+}
---
+++ tests/testthat/test-frq_filter.R
@@ -0,0 +1,29 @@
+test_that("Filter SNPs based on FRQ", {
+    file <- tempfile()
+    # write the Educational Attainment GWAS to a temp file for testing
+    eduAttainOkbay <- readLines(system.file("extdata", "eduAttainOkbay.txt",
+        package = "MungeSumstats"
+    ))
+    writeLines(eduAttainOkbay, con = file)
+    # read it in and make N
+    sumstats_dt <- data.table::fread(file, nThread = 1)
+    # Add N column and make it not an integer
+    set.seed(101)
+    sumstats_dt[, FRQ := runif(nrow(sumstats_dt)) * 2]
+    # get SNPs with INFO<0.9
+    rmv_snps <- sumstats_dt[FRQ < 0.9, ]$MarkerName
+    data.table::fwrite(x = sumstats_dt, file = file, sep = "\t")
+    # Run MungeSumstats code
+    reformatted <- MungeSumstats::format_sumstats(file,
+        ref_genome = "GRCh37",
+        FRQ_filter = 0.9,
+        on_ref_genome = FALSE,
+        strand_ambig_filter = FALSE,
+        bi_allelic_filter = FALSE,
+        allele_flip_check = FALSE,
+        log_folder_ind = TRUE,
+        imputation_ind = TRUE
+    )
+    res_dt <- data.table::fread(reformatted$sumstats, nThread = 1)
+    testthat::expect_equal(all(!rmv_snps %in% res_dt$SNP), TRUE)
+})
