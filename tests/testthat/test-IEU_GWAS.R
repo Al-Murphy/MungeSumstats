95474716e8f033cb4b1768023e5d437fc3c6cd91
Al-Murphy <alanmurph94@hotmail.com>
2021-10-04 11:49:15 +0100

    GHA issues

--- tests/testthat/test-IEU_GWAS.R
+++ tests/testthat/test-IEU_GWAS.R
@@ -69,7 +69,8 @@ test_that("Test connection to IEU GWAS - metadata, download", {
         ),
         error = function(e) e, warning = function(w) w
         )
-    testthat::expect_true(is(axel_catch, "warning"))
+    testthat::expect_true(is(axel_catch, "warning")||"warning" %in% 
+                                                            class(axel_catch))
 
     # don't run last check too time intensive, it is also in the vignette anyway
     # reformatted <- MungeSumstats::import_sumstats(ids = ids[1],
