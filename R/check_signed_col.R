a9b1d0448e3ec4a13788e9fb89b5a060d58d3f44
sjfandrews <s.j.f.andrews@gmail.com>
2022-02-23 10:17:33 -0500

    negate %in%

--- R/check_signed_col.R
+++ R/check_signed_col.R
@@ -22,7 +22,7 @@ check_signed_col <- function(sumstats_dt) {
     col_headers <- names(sumstats_dt)
     signed_stat_column_names <- c("Z", "OR", "BETA",
                                   "LOG_ODDS", "SIGNED_SUMSTAT")
-    
+    `%nin%` = negate(`%in%`)
     stp_msg <- paste0(
         "ERROR: cannot find a column name representing signed ",
         "statistic in GWAS sumstats file:\n",
