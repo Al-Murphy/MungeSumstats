3b59105b8849be2aa76ff9408701f70602f5ed07
Al-Murphy <alanmurph94@hotmail.com>
2025-08-25 17:10:21 -0400

    Remove unnecessary internet connection test

--- longtests/testthat/test-zzz.R
+++ longtests/testthat/test-zzz.R
@@ -10,15 +10,15 @@ test_that("zzz works", {
         library(MungeSumstats)
         opts <- options()
         
-        testthat::expect_equal(opts$googleAuthR.batch_endpoint,
-                               "https://www.googleapis.com/batch")
+        #testthat::expect_equal(opts$googleAuthR.batch_endpoint,
+        #                       "https://www.googleapis.com/batch")
         testthat::expect_equal(opts$gargle_oauth_cache,
                                "ieugwasr_oauth")
         testthat::expect_equal(opts$googleAuthR.webapp.client_secret,
                                "I7Gqp83Ku4KJxL9zHWYxG_gD")
     }
     else{
-        expect_equal(TRUE, TRUE)
+        #expect_equal(TRUE, TRUE)
         expect_equal(TRUE, TRUE)
         expect_equal(TRUE, TRUE)
     }
